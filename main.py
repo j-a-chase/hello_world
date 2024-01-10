96e93cbe18c3a94f2fe4ff119b7c32bbe6a4031f
James Chase <gochases@gmail.com>
2024-01-10 10:40:05 -0700

    Add main.py, implement simple hello world program

---
+++ main.py
@@ -0,0 +1,18 @@
+######################################################################################################################################################
+# Name: James A. Chase
+# File: main.py
+# Date: 10 January 2024
+# Description:
+#
+# Simple program that prints 'Hello world!' to the console.
+#
+######################################################################################################################################################
+
+# main function
+def main() -> None:
+    print()
+    print('Hello world!')
+    print()
+
+# run if main file
+if __name__ == '__main__': main()
