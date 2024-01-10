e211962fcd9872d7f991f6239aedde9a17c9a2c6
James Chase <gochases@gmail.com>
2024-01-10 10:56:16 -0700

    Add increased complexity to hello world program, requiring the user to play a guessing game to print it.

--- main.py
+++ main.py
@@ -8,11 +8,57 @@
 #
 ######################################################################################################################################################
 
-# main function
-def main() -> None:
+# imports
+from random import randint
+
+# define constants
+SECRET_NUM = randint(1, 100)
+
+def guess_the_number() -> None:
+    '''
+    Makes user play a simple guessing number game to print 'Hello world!' to the console.
+
+    Parameters: None
+
+    Returns: None
+    '''
+    guess = 0
+    print('Guess the number [1-100] --')
+    while guess != SECRET_NUM:
+        try:
+            guess = int(input('> '))
+        except ValueError:
+            print('\tInvalid input format. Must be integer.')
+            guess = 0
+            continue
+
+        if guess == SECRET_NUM:
+            print_msg()
+            break
+        print('\tToo high!') if guess > SECRET_NUM else print('\tToo low!')
+
+def print_msg() -> None:
+    '''
+    Prints 'Hello world!' to the console.
+
+    Parameters: None
+
+    Returns: None
+    '''
     print()
     print('Hello world!')
     print()
+
+# main function
+def main() -> None:
+    '''
+    Main Function
+
+    Parameters: None
+
+    Returns: None
+    '''
+    guess_the_number()
 
 # run if main file
 if __name__ == '__main__': main()
